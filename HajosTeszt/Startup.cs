98deccd03295ec21a46434aa9d8abba680b783cb
Barnabás Horváth <barnabas.horvath3@stud.uni-corvinus.hu>
2021-05-07 18:07:11 +0200

    8. gyakorlat

--- HajosTeszt/Startup.cs
+++ HajosTeszt/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,37 +13,42 @@ namespace HajosTeszt
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
-            app.UseStaticFiles();
-
+            app.UseStaticFiles(); //A sorrend fontos!
 
             app.UseRouting();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    //await context.Response.WriteAsync("Hello World!");
-                    app.UseHttpsRedirection();
-                    app.UseDefaultFiles();
-                    app.UseStaticFiles();
-                });
+                endpoints.MapControllers();
             });
         }
+
+
+
+
     }
 }
